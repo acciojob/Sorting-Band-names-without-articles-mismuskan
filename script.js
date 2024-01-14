2cff3b5162478c01c4d333faa21d0312ebdead8b
Muskan Sheikh <144351188+mismuskan@users.noreply.github.com>
2024-01-14 18:26:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 //your code here
+  let bandNames = ['The Rolling Stones', 'Led Zeppelin', 'The Beatles', 'Queen', 'Aerosmith'];
 
+    // Remove articles ('a', 'an', 'the') and sort the band names
+    let sortedBandNames = bandNames.map(name => name.replace(/^(a|an|the)\s/i, '').trim()).sort();
+
+    // Create HTML list
+    let ul = document.createElement('ul');
+    ul.id = 'band';
+
+    sortedBandNames.forEach(name => {
+        let li = document.createElement('li');
+        li.textContent = name;
+        ul.appendChild(li);
+    });
+
+    // Append the list to the body
+    document.body.appendChild(ul);
